a7aa76b82cb3a815f3908e4a85ab477267c623ec
Clemeleon <clem@clemeleon.com>
2021-04-12 23:51:44 +0300

    pages route added and package json cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  RouteComponentProps,
+  RouteProps,
+  match,
+  Redirect,
+} from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Header } from "./components/Header";
 import { Render } from "./helpers/types";
 import { Footer } from "./components/Footer";
 import { StoreContext } from "./components/stores/Store";
+import { Carts } from "./pages/Carts";
+import { Products } from "./pages/Products";
+import OneProduct from "./pages/OneProduct";
+import { Error } from "./pages/Error";
 
-class App extends Component {
+type ProductParams = { name: string };
+
+type ProductRouteParams = { match?: match<ProductParams> };
+
+type AppProps = {};
+
+type AppStates = {};
+
+class App extends Component<AppProps, AppStates> {
   public static contextType = StoreContext;
+
+  private static product({ match }: ProductRouteParams) {
+    if (match) {
+      const { params } = match,
+        { name } = params;
+      if (name.length > 0) {
+        return <OneProduct  name={name}/>;
+      }
+    }
+    return <Redirect to={"/"} />;
+  }
+
   public render(): Render {
     return (
       <div className="App">
@@ -16,6 +48,13 @@ class App extends Component {
           <Switch>
             <Route exact path="/">
               {<Home />}
+            </Route>
+            <Route exact path="/products">
+              {<Products />}
+            </Route>
+            <Route exact path="/product/:name" render={App.product} />
+            <Route exact path="/carts">
+              {<Carts />}
             </Route>
           </Switch>
           <Footer />
